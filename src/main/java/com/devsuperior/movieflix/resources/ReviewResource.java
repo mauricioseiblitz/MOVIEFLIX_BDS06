df4874c752b1063dfb3792cbcde06fd1c894d059
mackristofer <mackristofer@gmail.com>
2022-10-28 11:32:42 -0300

    Teste ok

--- src/main/java/com/devsuperior/movieflix/resources/ReviewResource.java
+++ src/main/java/com/devsuperior/movieflix/resources/ReviewResource.java
@@ -6,8 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +23,12 @@ public class ReviewResource {
 	@Autowired
 	private ReviewService service;
 	
+	@PreAuthorize("hasAnyRole('MEMBER')")
 	@PostMapping
 	public ResponseEntity<ReviewDTO> insert(@Valid @RequestBody ReviewDTO dto) {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(dto.getId()).toUri();
+		        .buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
 }
